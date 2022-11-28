f856d175e7c226422a63c138a59c77c5015e2d0a
santiagoVL03 <santiagovl0308@gmail.com>
2022-11-28 09:51:03 -0500

    app_web

---
+++ js/s_t_micro.js
@@ -0,0 +1,54 @@
+var phraseDiv;
+var startRecognizeOnceAsyncButton;
+
+var subscriptionKey, serviceRegion;
+var SpeechSDK;
+var recognizer;
+
+document.addEventListener("DOMContentLoaded", function () {
+  startRecognizeOnceAsyncButton = document.getElementById("startRecognizeOnceAsyncButton");
+  subscriptionKey = "a6bf0523dd554d9ea768ec19b7ab7d1c";
+  serviceRegion = "eastus";
+  phraseDiv = document.getElementById("phraseDiv");
+
+  startRecognizeOnceAsyncButton.addEventListener("click", function () {
+    startRecognizeOnceAsyncButton.disabled = true;
+    phraseDiv.innerHTML = "";
+
+    if (subscriptionKey === "" || subscriptionKey === "subscription") {
+      alert("Consigue una clave de suscripcion ^w^!");
+      return;
+    }
+    var speechConfig = SpeechSDK.SpeechConfig.fromSubscription(subscriptionKey, serviceRegion);
+
+    speechConfig.speechRecognitionLanguage = "es-MX";
+    var audioConfig  = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
+    recognizer = new SpeechSDK.SpeechRecognizer(speechConfig, audioConfig);
+
+    recognizer.recognizeOnceAsync(
+      function (result) {
+        startRecognizeOnceAsyncButton.disabled = false;
+        phraseDiv.innerHTML += result.text;
+        window.console.log(result);
+
+        recognizer.close();
+        recognizer = undefined;
+      },
+      function (err) {
+        startRecognizeOnceAsyncButton.disabled = false;
+        phraseDiv.innerHTML += err;
+        window.console.log(err);
+
+        recognizer.close();
+        recognizer = undefined;
+      });
+  });
+
+  if (!!window.SpeechSDK) {
+    SpeechSDK = window.SpeechSDK;
+    startRecognizeOnceAsyncButton.disabled = false;
+
+    document.getElementById('content').style.display = 'block';
+    document.getElementById('warning').style.display = 'none';
+  }
+});
